c018fd50dcc0bd9e213bee0abab44466dc89e0f9
khakha93 <rkwowns@naver.com>
2024-09-02 11:33:33 +0900

    파라미터 입력 자동화

--- crawling/kha/naverNews.py
+++ crawling/kha/naverNews.py
@@ -7,6 +7,7 @@ from dateutil import rrule
 
 import pandas as pd
 import json
+import os
 
 def get_info(soup:BeautifulSoup):
     info_list = []
@@ -22,11 +23,12 @@ def get_info(soup:BeautifulSoup):
             info_list.append([title, press, news_url])
     return info_list
 
-def scrap_pages(date_str, headers):
+def scrap_pages(date_str, sid2):
+    headers = {'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/127.0.0.0 Safari/537.36'}
     info_list = []
 
     # 첫 화면
-    init_url = f"https://news.naver.com/breakingnews/section/101/259?date={date_str}"
+    init_url = f"https://news.naver.com/breakingnews/section/101/{sid2}?date={date_str}"
     res = requests.get(init_url, headers= headers)
     soup = BeautifulSoup(res.text, 'html.parser') 
     info_list.extend(get_info(soup))
@@ -36,7 +38,7 @@ def scrap_pages(date_str, headers):
         try:
             p_next = soup.select_one('div.section_latest_article')['data-cursor']
             p_time = calendar.timegm(dt.datetime.now().timetuple())
-            next_url = f"https://news.naver.com/section/template/SECTION_ARTICLE_LIST_FOR_LATEST?sid=101&sid2=259&cluid=&pageNo={i}&date={date_str}&next={p_next}&_={p_time}"
+            next_url = f"https://news.naver.com/section/template/SECTION_ARTICLE_LIST_FOR_LATEST?sid=101&sid2={sid2}&cluid=&pageNo={i}&date={date_str}&next={p_next}&_={p_time}"
         except TypeError:
             return info_list
         
@@ -49,11 +51,50 @@ def scrap_pages(date_str, headers):
     
     return info_list
 
-headers = {'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/127.0.0.0 Safari/537.36'}
 
-date_str = '20240731'
-info_list = scrap_pages(date_str, headers)
+if __name__=='__main__':
 
-for i, row in enumerate(info_list):
-    title, press, news_url = row
-    print(i, title)
+    sec2_dict = {'증권': 258, '금융': 259, '부동산': 260, '산업/재계': 261,
+                 '글로벌 경제': 262, '경제 일반': 263, '생활경제': 310, '중기/벤처': 771}
+    MAX_DATA = int('200,000'.replace(',', ''))
+    cnt = 0
+
+    std_date = pd.to_datetime('2024-08-01')
+
+    # 날짜 거꾸로 타고 가기
+    while cnt < MAX_DATA:
+        # 월별 구분
+        std_date = std_date - pd.DateOffset(months=1)
+        print(f"{std_date} Month start!")
+
+        # 일별 구분
+        for d in range(1, 32):
+            try:
+                now_date = dt.datetime(std_date.year, std_date.month, d)
+            except ValueError:
+                break
+            print(f"{now_date} Day start!")
+
+            df_day= pd.DataFrame()
+
+            # 특정 날짜에서 기사(하위 카테고리 전체) 가져오기
+            date_str = now_date.strftime(r'%Y%m%d')
+            for sec2, sid2 in sec2_dict.items():
+                info_list = scrap_pages(date_str, sid2)
+                df_day_sec2 = pd.DataFrame(info_list, columns=['title', 'press', 'url'])
+                df_day_sec2['date'] = now_date.strftime(r'%Y-%m-%d')
+                df_day_sec2['sec2'] = sec2
+
+                df_day = pd.concat([df_day, df_day_sec2], ignore_index=True)
+                print(f"Sector: {sec2}, {len(df_day_sec2)}개")
+            cnt += len(df_day)
+        
+            # 최초 생성 이후 mode는 append
+            if not os.path.exists('news_naver.csv'):
+                df_day.to_csv('news_naver.csv', index=False, mode='w')
+            else:
+                df_day.to_csv('news_naver.csv', index=False, mode='a', header=False)
+            print(f"{now_date} Day finished! -> now: {cnt}개")
+            break
+        print(f"{std_date} Month finished! -> now: {cnt}개")
+        break
