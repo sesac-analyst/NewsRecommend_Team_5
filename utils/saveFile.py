499039279c19c99cf98c7b7d6bdac0460100f7ca
khakha93 <rkwowns@naver.com>
2024-09-05 14:22:58 +0900

    Branch 정리 전

--- utils/saveFile.py
+++ utils/saveFile.py
@@ -14,8 +14,8 @@ def save_DF(df: pd.DataFrame, file_path: str):
     # 최초 생성 이후 mode는 append
     if not os.path.exists(file_path):
         df.to_csv(file_path, index=False, mode='w')
-        print(f"Write {len(df)}개 in {file_path}")
+        # print(f"Write {len(df)}개 in {file_path}")
     else:
         df.to_csv(file_path, index=False, mode='a', header=False)
-        print(f"Add {len(df)}개 in {file_path}")
+        # print(f"Add {len(df)}개 in {file_path}")
     return
